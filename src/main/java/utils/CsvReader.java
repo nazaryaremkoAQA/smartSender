4039866adffc30b14134fec05a331019fbee8219
nyaremko <nyaremko@modeln.com>
2022-10-30 12:36:36 +0200

    refactoring CsvReader

--- src/main/java/utils/CsvReader.java
+++ src/main/java/utils/CsvReader.java
@@ -1,55 +1,20 @@
 package utils;
 
 import com.opencsv.bean.*;
-import pojo.*;
 
 import java.io.*;
 import java.util.*;
 
 public class CsvReader {
 
-
-    public static List<ContactData> getContactData(String fileName, String value)   {
+    public List getCsvData(String fileName, String value, Class clazz)   {
         try {
-            return  new CsvToBeanBuilder<ContactData>(new FileReader(fileName))
-                    .withType(ContactData.class)
+            return new CsvToBeanBuilder<>(new FileReader(fileName))
+                    .withType(clazz)
                     .withFilter(id -> id[0].equals(value))
                     .build().parse();
         } catch (FileNotFoundException e) {
             return null;
         }
      }
-
-     public static List<Contact> getContact(String fileName, String value)   {
-        try {
-            return  new CsvToBeanBuilder<Contact>(new FileReader(fileName))
-                    .withType(Contact.class)
-                    .withIgnoreEmptyLine(true)
-                    .withFilter(id -> id[0].equals(value))
-                    .build().parse();
-        } catch (FileNotFoundException e) {
-            return null;
-        }
-     }
-
-    public static List<Contact.Variables> getVariables(String fileName, String value)   {
-        try {
-            return  new CsvToBeanBuilder<Contact.Variables>(new FileReader(fileName))
-                    .withType(Contact.Variables.class)
-                    .withFilter(id -> id[0].contains(value))
-                    .build().parse();
-        } catch (FileNotFoundException e) {
-            return null;
-        }
-    }
-    public static List<Contact.Cac> getCacCSV(String fileName, String value)   {
-        try {
-            return  new CsvToBeanBuilder<Contact.Cac>(new FileReader(fileName))
-                    .withType(Contact.Cac.class)
-                    .withFilter(id -> id[0].equals(value))
-                    .build().parse();
-        } catch (FileNotFoundException e) {
-            return null;
-        }
-    }
 }
